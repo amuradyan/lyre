6ec70fbc9ea8eb5cae706d43a4116b9c1eea7021
darthspectrum <muradianator@gmail.com>
2025-08-15 11:55:04 +0400

    :broom:

--- react/src/StatusMessage.jsx
+++
@@ -1,9 +0,0 @@
-export default function StatusMessage({ allTestsPassed }) {
-  return (
-    <div style={{ fontSize: '0.875rem', color: '#6b7280', marginTop: '8px' }}>
-      {allTestsPassed
-        ? 'All tests passed! You can proceed to the next chapter.'
-        : 'Complete all tests to unlock the next chapter'}
-    </div>
-  );
-}
