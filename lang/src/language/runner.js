cfa34e0433677a5fa04acea96544be713eea9838
darthspectrum <muradianator@gmail.com>
2026-03-14 04:03:45 +0400

    🩹 Silent padding added to prevent audio driver clipping

--- lang/src/language/runner.js
+++ lang/src/language/runner.js
@@ -6,10 +6,20 @@ export function* stream(filePath) {
   const code = readFileSync(filePath, 'utf-8');
   const tokens = desugar(tokenize(code));
 
+  const padding = 4800;
+
+  for (let i = 0; i < padding; i++) {
+    yield [0, 0, 0];
+  }
+
   for (const expr of tokens) {
     const generator = interpret(expr);
     for (const sample of generator) {
       yield sample;
     }
+  }
+
+  for (let i = 0; i < padding; i++) {
+    yield [0, 0, 0];
   }
 }
